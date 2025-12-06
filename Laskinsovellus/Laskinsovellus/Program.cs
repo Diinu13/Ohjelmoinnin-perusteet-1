88589ec72ba4db5d9f5cbc58abd86e172baaef30
Diinu13 <markus.rah@gmail.com>
2025-12-06 22:59:09 +0200

    Laskinsovellus

---
+++ Laskinsovellus/Laskinsovellus/Program.cs
@@ -0,0 +1,40 @@
+﻿// Käytä "," desimaaleissa!!!!!!
+Console.Write("Kirjoita numero: ");
+double numero1 = double.Parse(Console.ReadLine());
+Console.Write("Kirjoita toinen numero: ");
+double numero2 = double.Parse(Console.ReadLine());
+Console.Write("Käytä jotain näistä: - + * / operaattoreista");
+string operaatio = Console.ReadLine();
+
+if (operaatio == "/" && numero2 == 0)
+{
+    Console.WriteLine("Nollala ei voida jakaa!");
+    return;
+}
+double tulos = 0;
+
+if (operaatio == "+")
+{
+    tulos = numero1 + numero2;
+}
+else if (operaatio == "-")
+{
+    tulos = numero1 - numero2;
+}
+else if (operaatio == "/")
+{
+    tulos = numero1 / numero2;
+}
+else if (operaatio == "*")
+{
+    tulos = numero1 * numero2;
+}
+else
+{
+    Console.WriteLine("Tuntematon operaatio!");
+    return;
+}
+
+Console.WriteLine($"{numero1} {operaatio} {numero2} = {tulos}");
+
+
