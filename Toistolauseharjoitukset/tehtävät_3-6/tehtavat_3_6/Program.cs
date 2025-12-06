ce5a544eb9a83462013ed02941199f139242433f
Diinu13 <markus.rah@gmail.com>
2025-12-06 16:14:31 +0200

    tehtävät 3 ja 4

---
+++ Toistolauseharjoitukset/tehtävät_3-6/tehtavat_3_6/Program.cs
@@ -0,0 +1,36 @@
+﻿// Harjoitus 3 
+
+string [] sanat = new string [3];
+
+string sana;
+for (int i = 0; i < 3; i++)
+{
+    Console.Write("kirjoita kolme sanaa: ");
+    sana = Console.ReadLine();
+    sanat[i] = sana;
+}
+for (int i = 2; i>= 0; i--)
+{
+    Console.WriteLine(sanat[i]);
+}
+// Harjoitus 4
+{
+string [] sanat4 = new string [3];
+
+int count = 0;
+
+while (count < sanat4.Length)
+
+{
+    Console.Write("kirjoita kolme sanaa: ");
+    string sana4 = Console.ReadLine();
+    sanat4[count] = sana4;
+    count++;
+}
+int index = sanat4.Length -1;
+while (index >= 0)
+{
+    Console.WriteLine(sanat4[index]); 
+    index--;
+}
+}
